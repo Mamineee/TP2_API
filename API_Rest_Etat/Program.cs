c2cd53f93e7bd99a0be978b4d85dbbb0a1bff99c
Mohamed-Amin <mamine22123@gmail.com>
2024-01-30 16:11:11 +0100

    enfin changement bdd

--- API_Rest_Etat/Program.cs
+++ API_Rest_Etat/Program.cs
@@ -1,6 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using API_Rest_Etat.Models.EntityFramework;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<SeriesDbContext>(options => 
+    options.UseNpgsql(builder.Configuration.GetConnectionString("SeriesDbContextRemote")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -16,7 +21,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
